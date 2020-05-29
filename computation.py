d6d9c84f227ec311507df269b7e8ce9dcfac3a4a
Camron Staley <55006027+CamronStaley@users.noreply.github.com>
2020-05-29 09:58:35 -0700

    blastn setup waiting for correct covid file

--- computation.py
+++ computation.py
@@ -1,10 +1,14 @@
 import os
+directory = 'C:/Users/camro/PycharmProjects/BIO383_P2/data/'
 
-curr_dir = 'C:/Users/camro/PycharmProjects/BIO383_P2'
 
-def compare_dir(file, directory):
-    this_path = curr_dir + '/' + directory + '/' + file
-    for filename in os.listdir(directory):
-        other_path = curr_dir + '/' + directory + '/' + filename
-        os.system('blastn -query ' + this_path + ' -subject ' + other_path + ' -task blastn -max_hsps 2 -outfmt 6 >> '
-                                                                             'file_results.txt')
+def compare(this, that):
+    name = that
+    that = directory + that
+    os.putenv("BLASTDB_LMDB_MAP_SIZE", "1000000")
+    firstCommand = "makeblastdb -in {this_file} -input_type fasta -dbtype nucl"
+    firstCommand = firstCommand.format(this_file=this)
+    os.system(firstCommand)
+    output = 'blastn -query {that_file} -subject {this_file} -task blastn -max_hsps 2 -outfmt 6 >> {name_file}_results.txt'
+    output = output.format(that_file=that, this_file=this, name_file=name)
+    os.system(output)
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import computation
+import os
 
-computation.compare_dir('COVID19.fasta', 'data')
+
+query = 'C:/Users/camro/PycharmProjects/BIO383_P2/COVID19.fasta'
+directory = 'C:/Users/camro/PycharmProjects/BIO383_P2/data'
+for file in os.listdir(directory):
+    if 'results.txt' not in file:
+        print(file)
+        computation.compare(query, file)
