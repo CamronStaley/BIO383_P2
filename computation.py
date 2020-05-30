ce4e96e2601f5f20c86f9abce77e8e4316fe0202
Camron Staley <55006027+CamronStaley@users.noreply.github.com>
2020-05-30 13:12:07 -0700

    getting data and fomatting into lists for dislaying in bar chart

--- computation.py
+++ computation.py
@@ -6,9 +6,9 @@ def compare(this, that):
     name = that
     that = directory + that
     os.putenv("BLASTDB_LMDB_MAP_SIZE", "1000000")
-    firstCommand = "makeblastdb -in {this_file} -input_type fasta -dbtype nucl"
-    firstCommand = firstCommand.format(this_file=this)
-    os.system(firstCommand)
-    output = 'blastn -query {that_file} -subject {this_file} -task blastn -max_hsps 2 -outfmt 6 >> {name_file}_results.txt'
+    create_db = "makeblastdb -in {this_file} -input_type fasta -dbtype nucl"
+    create_db = create_db.format(this_file=this)
+    os.system(create_db)
+    output = "blastn -query {that_file} -subject {this_file} -task blastn -max_hsps 5 -outfmt 6 >> {name_file}_results.txt"
     output = output.format(that_file=that, this_file=this, name_file=name)
     os.system(output)
