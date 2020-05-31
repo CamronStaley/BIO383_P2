64ad54715ede55d002c59d794173d5a29640725d
Camron Staley <55006027+CamronStaley@users.noreply.github.com>
2020-05-30 19:39:40 -0700

    change output

--- computation.py
+++ computation.py
@@ -9,6 +9,6 @@ def compare(this, that):
     create_db = "makeblastdb -in {this_file} -input_type fasta -dbtype nucl"
     create_db = create_db.format(this_file=this)
     os.system(create_db)
-    output = "blastn -query {that_file} -subject {this_file} -task blastn -max_hsps 5 -outfmt 6 >> {name_file}_results.txt"
+    output = "blastn -query {that_file} -subject {this_file} -task blastn -max_hsps 1 -outfmt 6 >> {name_file}_results.txt"
     output = output.format(that_file=that, this_file=this, name_file=name)
     os.system(output)
