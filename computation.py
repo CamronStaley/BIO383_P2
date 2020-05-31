c4ebdfbb2787ff11e3299c54b95b7f3733803641
Conway-Sabini <ConwaySabini@protonmail.com>
2020-05-30 19:35:57 -0700

    graph

--- computation.py
+++ computation.py
@@ -1,5 +1,5 @@
 import os
-directory = 'C:/Users/camro/PycharmProjects/BIO383_P2/data/'
+directory = 'C:/Users/econw/Documents/biotest/data/'
 
 
 def compare(this, that):
@@ -9,6 +9,6 @@ def compare(this, that):
     create_db = "makeblastdb -in {this_file} -input_type fasta -dbtype nucl"
     create_db = create_db.format(this_file=this)
     os.system(create_db)
-    output = "blastn -query {that_file} -subject {this_file} -task blastn -max_hsps 5 -outfmt 6 >> {name_file}_results.txt"
+    output = "blastn -query {that_file} -subject {this_file} -task blastn -max_hsps 1 -outfmt 6 >> {name_file}_results.txt"
     output = output.format(that_file=that, this_file=this, name_file=name)
     os.system(output)
